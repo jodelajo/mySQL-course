161ef938e6c1abc1056c88ee0a7d0af97ed43d88
Joanneke Haasbroek <joanneke.haasbroek@opencii.nl>
2023-02-22 15:45:47 +0100

    online shop exercise

--- 18-online-shop.sql
+++ 18-online-shop.sql
@@ -2,7 +2,7 @@
 
 -- CREATE TABLE  products (
 --     product_name VARCHAR(200),
---     product_price DECIMAL(10,2),
+--     product_price DECIMAL(10,2), <--should be NUMERIC
 --     description TEXT,
 --     amount INT,
 --     image VARCHAR(400)
@@ -18,8 +18,21 @@
 -- VALUES ('brommer', 1499.99, 'Hele mooie brommer', 80, 'https://www.alblasserdamsnieuws.nl/wordpress/wp-content/uploads/2019/06/6b3e5w9-Large.jpg');
 
 -- mysql
+-- ALTER TABLE products
+-- MODIFY COLUMN product_name VARCHAR(200) NOT NULL,
+-- MODIFY COLUMN product_price DECIMAL(10,2) NOT NULL,
+-- MODIFY COLUMN amount INT NOT NULL;
+
+-- postgres
+-- ALTER TABLE products
+-- ALTER COLUMN product_name SET NOT NULL,
+-- ALTER COLUMN product_price SET NOT NULL,
+-- ALTER COLUMN amount SET NOT NULL,
+-- ADD CONSTRAINT amount_postive CHECK (amount > 0);
+
+
+ -- id INT PRIMARY KEY AUTO_INCREMENT,
+--     id SERIAL PRIMARY KEY,
+
 ALTER TABLE products
-MODIFY product_name NOT NULL,
-MODIFY product_price NOT NULL,
-MODIFY amount NOT NULL,
-ADD CONSTRAINT amount_postive CHECK (amount > 0);
+ADD COLUMN id SERIAL PRIMARY KEY; 
