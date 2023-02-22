34e7f8f4b199966d23b24d3fb4ce1c80374d4a87
Joanneke Haasbroek <joanneke.haasbroek@opencii.nl>
2023-02-22 15:47:27 +0100

    last file update

--- 17-primary-key.sql
+++ 17-primary-key.sql
@@ -1,25 +1,24 @@
-DROP TABLE users;
--- DROP TABLE employers;
+-- DROP TABLE users;
+DROP TABLE employers;
 -- DROP TABLE conversations;
 
 
-CREATE TABLE users (
-    -- id INT PRIMARY KEY AUTO_INCREMENT,
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(300) NOT NULL,
-    yearly_salary INT CHECK (yearly_salary > 0),
-    current_status ENUM('employed', 'self-employed', 'un-employed')
---    current_status employment_status
-);
-
--- CREATE TABLE employers (
+-- CREATE TABLE users (
 --     -- id INT PRIMARY KEY AUTO_INCREMENT,
---      id SERIAL PRIMARY KEY,
---     company_name VARCHAR(300) NOT NULL,
---     company_address VARCHAR(300) NOT NULL,
---     yearly_revenue FLOAT CHECK (yearly_revenue > 0),
---     is_hiring BOOLEAN DEFAULT FALSE
+--     id SERIAL PRIMARY KEY,
+--     full_name VARCHAR(300) NOT NULL,
+--     yearly_salary INT CHECK (yearly_salary > 0),
+--     current_status ENUM('employed', 'self-employed', 'un-employed')
+-- --    current_status employment_status
 -- );
+
+CREATE TABLE employers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    company_name VARCHAR(300) NOT NULL,
+    company_address VARCHAR(300) NOT NULL,
+    yearly_revenue FLOAT CHECK (yearly_revenue > 0),
+    is_hiring BOOLEAN DEFAULT FALSE
+);
 
 -- CREATE TABLE conversations (
 --     -- id INT PRIMARY KEY AUTO_INCREMENT,
